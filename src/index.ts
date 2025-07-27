3138e50961a8da234f573187982ae434f0233b25
Georgios Tselikas <georgios.p.tselikas@gmail.com>
2025-07-27 22:10:51 +0300

    initial commit: app initialization, users schema, users model added

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors'
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(cors({
+  credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app)
+
+server.listen(3000, () => { console.log('Server is running on port http://localhost:3000/'); });
+
+const MONGO_URL = "mongodb+srv://admin:admin@cluster0.gfln2u6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
