9a63df15d38808fcf4f1b827cc2f383b6f6b8813
Georgios Tselikas <georgios.p.tselikas@gmail.com>
2025-08-06 18:54:53 +0300

    feat: added client and supplier schema

---
+++ src/db/client.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema, model } from 'mongoose';
+
+const clientSchema = new mongoose.Schema({
+  user_id: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  phone: {
+    type: String,
+    trim: true
+  },
+  email: {
+    type: String,
+    trim: true
+  },
+  amount_owed: {
+    type: Number,
+    default: 0.0
+  },
+  notes: {
+    type: String
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export const ClientModel = mongoose.model('Client', clientSchema);
