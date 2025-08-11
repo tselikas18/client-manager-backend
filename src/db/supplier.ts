0d295bc7ccfc6085e32a0d14f42ebf25e7fd2ada
Georgios Tselikas <georgios.p.tselikas@gmail.com>
2025-08-11 09:52:30 +0300

    fix: added db supplier controllers

--- src/db/supplier.ts
+++ src/db/supplier.ts
@@ -12,3 +12,15 @@ const supplierSchema = new mongoose.Schema({
 });
 
 export const SupplierModel = mongoose.model('Supplier', supplierSchema);
+
+//controllers
+export const getAllSuppliers = () => SupplierModel.find();
+export const getSupplierByName = (name: string) => SupplierModel.find({ name });
+export const getSupplierPhone = (phone: string) => SupplierModel.findOne({ phone });
+export const getSupplierEmail = (email: string) => SupplierModel.findOne({ email });
+export const createSupplier = (values: Record<string, any>) => new SupplierModel(values)
+    .save()
+    .then((suppliers) => suppliers.toObject());
+export const deleteSupplierById = (id: string) => SupplierModel.findOneAndDelete({ _id: id });
+export const updateSupplierById = (id: string, values: Record<string, any>) =>
+    SupplierModel.findOneAndUpdate({ _id: id }, values, { new: true });
