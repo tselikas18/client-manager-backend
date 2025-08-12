e5405b71ec08195f34cfe947a35b85ae7c2e414f
Georgios Tselikas <georgios.p.tselikas@gmail.com>
2025-08-12 17:27:53 +0300

    feat: added supplier and client router

--- src/router/indexRouter.ts
+++ src/router/indexRouter.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import authenticationRouter from "./authenticationRouter";
 import usersRouter from "./usersRouter";
+import supplierRouter from "./supplierRouter";
+import clientRouter from "./clientRouter";
 
 const router = express.Router();
 
 export default  (): express.Router => {
   authenticationRouter(router);
   usersRouter(router);
+  clientRouter(router);
+  supplierRouter(router);
 
   return router;
 }
