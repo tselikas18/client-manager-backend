1b35011500b03a86a71c105cf7e84bacaa6a744e
Georgios Tselikas <georgios.p.tselikas@gmail.com>
2025-08-13 16:28:37 +0300

    fix: changed routers function to correctly complete CRUD

--- src/router/clientRouter.ts
+++ src/router/clientRouter.ts
@@ -1,19 +1,23 @@
-import express from "express";
 import { isAuthenticated, isOwner } from "../middlewares";
-import { deleteClient, getAllClients, updateClient, createClient } from "../controllers/clientControllers";
-import { getClientByEmail, getClientByName, getClientByPhone} from "../db/client";
+import express from "express";
+import {
+  deleteClient,
+  getAllClients,
+  updateClient,
+  createClient,
+  getClientByPhoneController,
+  getClientByNameController,
+  getClientByEmailController
+} from "../controllers/clientControllers";
 
 export default (router: express.Router) => {
+  router.get("/client", isAuthenticated, isOwner, getAllClients);
 
-  router.get("/client", isAuthenticated, getAllClients);
-
-
-  router.get("/client/phone/:phone", isAuthenticated, isOwner, getClientByPhone);
-  router.get("/client/name/:name", isAuthenticated, isOwner, getClientByName);
-  router.get("/client/email/:email", isAuthenticated, isOwner, getClientByEmail);
-
+  router.get("/client/phone/:phone", isAuthenticated, isOwner, getClientByPhoneController);
+  router.get("/client/name/:name", isAuthenticated, isOwner, getClientByNameController);
+  router.get("/client/email/:email", isAuthenticated, isOwner, getClientByEmailController);
 
   router.delete("/client/:id", isAuthenticated, isOwner, deleteClient);
   router.patch("/client/:id", isAuthenticated, isOwner, updateClient);
-  router.post("/client/", isAuthenticated, isOwner, createClient);
+  router.post("/client/", isAuthenticated, createClient);
 };
